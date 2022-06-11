16306057ae6e0b892ee32fa7d2608b23d7781bb4
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-06-11 13:58:58 +0200

    started navigation

--- App.js
+++ App.js
@@ -1,27 +1,22 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { StyleSheet } from "react-native";
+// import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation";
+
+const Stack = createStackNavigator();
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<Text style={styles.header}>Zdravo Riade</Text>
-			<StatusBar style="auto" />
-			<View
-				style={{
-					justifyContent: "flex-start",
-					marginTop: 1,
-				}}
-			>
-				<Text>hello from my new text eee</Text>
-			</View>
-			<TextInput style={{ margin: 30 }} placeholder="enter message ..." />
-			<Button
-				onPress={() => {
-					alert("You tapped the button!");
-				}}
-				title="helo"
-			/>
-		</View>
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen
+					name="Home"
+					component={HomeScreen}
+					options={{ title: "Welcome" }}
+				/>
+				<Stack.Screen name="Profile" component={ProfileScreen} />
+			</Stack.Navigator>
+		</NavigationContainer>
 	);
 }
 
