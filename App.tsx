8e0e0313c5696d83934fec2870df53d60ecbdb2e
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-06-13 23:28:01 +0200

    add relative import paths

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Nav from './src/components/Nav';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app! test test tes</Text>
+      <Nav />
+      <Text>Open up App.tsx to start working on your app! test test stes</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/components/Nav.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import Test from 'src/components/Test';
+function Nav() {
+  return (
+    <View>
+      <Test />
+      <Text>Nav test lkjsadj ajjaj</Text>
+    </View>
+  );
+}
+
+export default Nav;
