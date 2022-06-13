4e71acfbd7e6a3053f1be5cbc40d87be9a1ad391
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-06-13 16:12:26 +0200

    finalize linting proces

--- App.tsx
+++ App.tsx
@@ -3,7 +3,6 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const test = 'adnan';
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
